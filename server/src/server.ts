e07644a1ff295b2a699ac99dc9a3ea5081999ddd
Edilson Andrade <edilson.a.andrade@gmail.com>
2020-12-16 19:53:41 -0300

    adding server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,8 +8,9 @@ import fileConfig from './config/fileConfig';
 import AppError from './error/AppError';
 
 const app = express();
-app.use(cors());
+
 app.use(express.json());
+app.use(cors());
 app.use('/files', express.static(fileConfig.directory));
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
